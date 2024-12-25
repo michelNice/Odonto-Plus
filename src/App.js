350412a2c9bc2ba9656269a9dea8c0fa760df9ff
michel <michel-gouveia1997@hotmail.com>
2024-12-25 09:56:37 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
 import Navbar from "./Components/Navbar/Navbar";
-
+import Hero from "./Components/Hero/Hero";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 };
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Hero.css'
+
+const Hero = ()=>{
+
+  return(
+
+    <>
+    <div className="p"></div>
+      <div className="hero">
+
+      </div>
+    </>
+  )
+}
+
+
+export default Hero
